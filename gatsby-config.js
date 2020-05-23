a3372ef6294f661caed38ed5a656b0733061876c
Salim Hamed <salimhamed@gmail.com>
2020-05-23 12:45:41 -0700

    Updat icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,13 +61,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `JODE Online`,
+        short_name: `JODE`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
